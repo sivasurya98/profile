6629b8871cda43b916cd3cb0941d19ce625e1678
sivasurya98 <133655652+sivasurya98@users.noreply.github.com>
2023-09-10 00:41:05 -0700

    profile basic

---
+++ profile/src/Component/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="navbar">
+        <a href="#">home</a>
+        <a href="#">Services</a>
+        <a href="#">portfolios</a>
+        <a href="#">Testimonials</a>
+        <div>
+         <button>Let's talk</button>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
